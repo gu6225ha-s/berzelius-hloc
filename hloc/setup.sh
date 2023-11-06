018aa2f48f2a883c73638ade36ca3243c285904f
gu6225ha-s <123593227+gu6225ha-s@users.noreply.github.com>
2023-11-06 16:10:21 +0100

    Update setup.sh

--- hloc/setup.sh
+++ hloc/setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# Exit on errors
-set -e
-# Create virtual environment
+# Create virtual environment (see https://www.nsc.liu.se/support/systems/berzelius-software/berzelius-pytorch/#installing-pytorch-via-pip)
+module load Mambaforge/23.3.1-1
 mamba create --name hloc python=3.8
 mamba activate hloc
 # Install hloc (see https://github.com/cvg/Hierarchical-Localization#installation)
 git clone --recursive https://github.com/cvg/Hierarchical-Localization/
 cd Hierarchical-Localization/
 python -m pip install -e .
-cd ..
