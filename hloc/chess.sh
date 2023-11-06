99579607c0ae474bc1a6c792c149775e643f5853
gu6225ha-s <123593227+gu6225ha-s@users.noreply.github.com>
2023-11-06 16:46:16 +0100

    Update chess.sh

--- hloc/chess.sh
+++ hloc/chess.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# To submit the job run "sbatch chess.sh"
+# For more details see https://www.nsc.liu.se/support/batch-jobs/introduction/
 #SBATCH -J hloc-chess
 #SBATCH -t 12:00:00
 #SBATCH --mem=2000
