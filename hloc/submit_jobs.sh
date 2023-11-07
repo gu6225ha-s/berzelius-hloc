24c0d2f26a73f39242885336a2b351e29eccf584
Gustav Hanning <gu6225ha-s@student.lu.se>
2023-11-07 08:18:46 +0100

    Submit one job for each scene.

---
+++ hloc/submit_jobs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for scene in chess fire heads office pumpkin redkitchen stairs
+do
+  cat <<EOT > $scene.sh
+#!/bin/bash
+#SBATCH -J hloc-$scene
+#SBATCH -t 12:00:00
+#SBATCH --mem=10000
+#SBATCH -n 2
+module load Mambaforge/23.3.1-1-hpc1-bdist
+mamba activate hloc
+python -m hloc.pipelines.7Scenes.pipeline --scenes chess
+EOT
+  chmod +x $scene.sh
+  echo "Submitting job for scene '$scene'"
+  sbatch $scene.sh
+done
+echo "Run 'squeue -u $USER' to see your jobs"
